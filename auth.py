a2cd5393737cf299cae85dbfb19e30b19fb1242e
NarasimhaChary <laxminarasimhachary4@gmail.com>
2026-06-28 21:33:05 +0530

    adding authentication

--- auth.py
+++ auth.py
@@ -10,7 +10,34 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm , OAuth2PasswordBearer
 from jose import jwt, JWTError
 
-router = APIRouter{
-    prefix='/auth'
+router = APIRouter(
+    prefix='/auth',
     tags=['auth']
-}
+)
+SECRET_KEY = 'b8d694ae8a58a9105f0cdadb2fe222b2a7aaba96d56153d6eab3f3cbc3de9926'
+ALGORITHM = 'HS256'
+
+bcrcypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_beare = OAuth2PasswordBearer(tokenUrl='auth/token')
+
+class CreateUserRequest(BaseModel):
+    username : str
+    password: str
+
+class Token(BaseModel):
+    access_token:str
+    token_type:str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/",status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
+    hashed_password = bcrcypt_context.hash(create_user_request.password)
